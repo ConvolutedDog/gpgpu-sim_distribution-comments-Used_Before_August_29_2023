43824b2bc2aecb4faefccea66e266a7ad9373967
ConvolutedDog <yangjianchao16@nudt.edu.cn>
2023-03-16 19:11:21 +0800

    Update stach.cc

--- src/gpgpu-sim/stack.cc
+++ src/gpgpu-sim/stack.cc
@@ -26,6 +26,10 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+/*
+stack.h/.cc 已停止使用。
+*/
+
 #include "stack.h"
 
 #include <assert.h>
