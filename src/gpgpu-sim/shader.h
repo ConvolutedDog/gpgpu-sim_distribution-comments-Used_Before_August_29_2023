cc9370752c69cbc9cdd98efa47245b3c8f39d5d6
masa-laboratory <masa-lab@outlook.com>
2023-05-11 09:24:18 +0800

    Update shader.h

--- src/gpgpu-sim/shader.h
+++ src/gpgpu-sim/shader.h
@@ -1182,35 +1182,51 @@ struct ifetch_buffer_t {
 
 class shader_core_config;
 
+/*
+simd_function_unit对象实现了SP单元和SFU单元（ALU流水线）。
+*/
 class simd_function_unit {
  public:
+  //构造函数。
   simd_function_unit(const shader_core_config *config);
   ~simd_function_unit() { delete m_dispatch_reg; }
 
   // modifiers
+  //issue(warp_inst_t*&)成员函数将给定的流水线寄存器的内容移入m_dispatch_reg。
   virtual void issue(register_set &source_reg);
   virtual void cycle() = 0;
+  //lane的意思为一个warp中有32个线程，而在流水线寄存器中可能暂存了很多条指令，这些指令的每对应的线程掩
+  //码的每一位都是一个lane。即遍历流水线寄存器中的非空指令，返回所有指令的整体线程掩码（所有指令线程掩
+  //码的或值）。
   virtual void active_lanes_in_pipeline() = 0;
 
   // accessors
   virtual unsigned clock_multiplier() const { return 1; }
+  //判断一条指令能否发射，即判断m_dispatch_reg是否为空，其在occupied对应的标识位是否为空。
   virtual bool can_issue(const warp_inst_t &inst) const {
     return m_dispatch_reg->empty() && !occupied.test(inst.latency);
   }
   virtual bool is_issue_partitioned() = 0;
+  //获取发射寄存器的ID。
   virtual unsigned get_issue_reg_id() = 0;
   virtual bool stallable() const = 0;
+  //打印SIMD单元的dispatch寄存器。
   virtual void print(FILE *fp) const {
     fprintf(fp, "%s dispatch= ", m_name.c_str());
     m_dispatch_reg->print(fp);
   }
+  //获取SIMD单元的名称。
   const char *get_name() { return m_name.c_str(); }
 
  protected:
+  //SIMD单元的名称。
   std::string m_name;
   const shader_core_config *m_config;
+  //SIMD单元的dispatch寄存器。
   warp_inst_t *m_dispatch_reg;
+  //最长的ALU指令的延迟，即流水线寄存器至多有512个槽。
   static const unsigned MAX_ALU_LATENCY = 512;
+  //流水线寄存器至多512个槽的位图，标识每个槽是否被占用。
   std::bitset<MAX_ALU_LATENCY> occupied;
 };
 
@@ -1229,7 +1245,7 @@ _core_ctx::execute()）。
 在每个pipelined_simd_unit中，issue(warp_inst_t*&)成员函数将给定的流水线寄存器的内容移入m_dispatch_
 reg。然后指令在m_dispatch_reg等待initiation_interval个周期。在此期间，没有其他的指令可以发到这个单
 元，所以这个等待是指令的吞吐量的模型。等待之后，指令被派发到内部流水线寄存器m_pipeline_reg进行延迟建
-模。派遣的位置是确定的，所以在m_dispatch_reg中花费的时间也被计入延迟中。每个周期，指令将通过流水线寄
+模。派发的位置是确定的，所以在m_dispatch_reg中花费的时间也被计入延迟中。每个周期，指令将通过流水线寄
 存器前进，最终进入m_result_port，这是共享的流水线寄存器，通向SP和SFU单元的共同写回阶段。
 
 各类指令的吞吐量和延迟在cuda-sim.cc的ptx_instruction::set_opcode_and_latency()中指定。这个函数在预
@@ -1243,7 +1259,11 @@ class pipelined_simd_unit : public simd_function_unit {
 
   // modifiers
   virtual void cycle();
+  //issue(warp_inst_t*&)成员函数将给定的流水线寄存器的内容移入m_dispatch_reg。
   virtual void issue(register_set &source_reg);
+  //lane的意思为一个warp中有32个线程，而在流水线寄存器中可能暂存了很多条指令，这些指令的每对应的线程掩
+  //码的每一位都是一个lane。即遍历流水线寄存器中的非空指令，返回所有指令的整体线程掩码（所有指令线程掩
+  //码的或值）。
   virtual unsigned get_active_lanes_in_pipeline();
 
   virtual void active_lanes_in_pipeline() = 0;
@@ -1257,10 +1277,12 @@ class pipelined_simd_unit : public simd_function_unit {
   */
   // accessors
   virtual bool stallable() const { return false; }
+  //判断一条指令能否发射，即判断m_dispatch_reg是否为空，其在occupied对应的标识位是否为空。
   virtual bool can_issue(const warp_inst_t &inst) const {
     return simd_function_unit::can_issue(inst);
   }
   virtual bool is_issue_partitioned() = 0;
+  //获取发射寄存器的ID。
   unsigned get_issue_reg_id() { return m_issue_reg_id; }
   virtual void print(FILE *fp) const {
     simd_function_unit::print(fp);
@@ -1273,10 +1295,14 @@ class pipelined_simd_unit : public simd_function_unit {
   }
 
  protected:
+  //流水线的深度。
   unsigned m_pipeline_depth;
+  //流水线寄存器。
   warp_inst_t **m_pipeline_reg;
+  //结果端口。
   register_set *m_result_port;
   class shader_core_ctx *m_core;
+  //发射寄存器的ID。
   unsigned m_issue_reg_id;  // if sub_core_model is enabled we can only issue
                             // from a subset of operand collectors
 
