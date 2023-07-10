1e0bdfe21ba80f100ed03062dbac904c783684d5
masa-laboratory <masa-lab@outlook.com>
2023-07-10 10:13:14 +0800

    Update build.sh

--- cuda_codes/wmma_tensorcore/build.sh
+++ cuda_codes/wmma_tensorcore/build.sh
@@ -1,4 +1,4 @@
-nvcc --compiler-options -fPIC --cudart shared -arch=sm_70 -o test test.cu
+nvcc --compiler-options -fPIC --cudart shared -arch=sm_70 --compiler-bindir /usr/bin/g++-5 -o test test.cu
 ldd test
 cp ../../configs/tested-cfgs/SM7_QV100/* ./
 source ../../setup_environment release
